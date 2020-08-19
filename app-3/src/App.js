51c4dd60bf7139ec90c55c6f08d38f44073766a7
Kyle Hansen <1hansen.kyle@gmail.com>
2020-08-19 17:27:48 -0600

    react drills 1-7

--- app-3/src/App.js
+++ app-3/src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+ class App extends Component {
+   constructor(){
+     super()
+
+     this.state = {
+       filterString: '',
+       candy: ['snickers', 'twix', 'life saver gummies', 'symphony bar', 'mike n ikes']
+     }
+   }
+   inputChange (filter) {
+     this.setState({filterString: filter})
+   }
+
+   render(){
+     let candy = this.state.candy.filter((element, index) => {
+       return element.includes(this.state.filterString)
+     })
+     .map((element, index) => {
+       return <h2 key={index}>{element}</h2>
+     })
+
+     return (
+       <div className='App'>
+         <input onChange={e => this.inputChange(e.target.value)} type='text'/> {candy}
+       </div>
+     )
+   }
+ 
 }
 
 export default App;
