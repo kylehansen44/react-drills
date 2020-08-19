b2fc82d5314eea7415fd2d20b33165529bfdf4a8
Kyle Hansen <1hansen.kyle@gmail.com>
2020-08-19 16:58:01 -0600

    react drills 1-7

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+class App extends Component {
+  constructor(){
+    super()
+
+    this.state = {
+      shows: ['The Office', 'Seinfeld', 'Arrested Development', 'Parks and Recreation', 'Psych']
+    }
+  }
+  render(){
+
+    let shows = this.state.shows.map((element, index) => {
+      return <h2 key={index}>{element}</h2>
+    })
+    return (
+      <div className="App">
+        <h2> {shows} </h2>
+      </div>
   );
+  }
 }
 
 export default App;
